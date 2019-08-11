32c9cbce8c99f415a3edb073190a9fdb6a528037
Patryk Płaczek <patryk.placzek@wp.pl>
2019-08-11 11:47:32 +0200

    Add removing active class scripts

--- js/script.js
+++ js/script.js
@@ -12,17 +12,29 @@ const titleClickHandler = function(event){
     console.log(event);
 
 
-    /* remove class 'active' from all article links  */
+/* remove class 'active' from all article links  */
 
-    /* add class 'active' to the clicked link */
+const activeLinks = document.querySelectorAll('.titles a.active');
 
-    /* remove class 'active' from all articles */
+for(let activeLink of activeLinks){
+  activeLink.classList.remove('active');
+}
 
-    /* get 'href' attribute from the clicked link */
+/* add class 'active' to the clicked link */
 
-    /* find the correct article using the selector (value of 'href' attribute) */
+/* remove class 'active' from all articles */
 
-    /* add class 'active' to the correct article */
+const activeArticles = document.querySelectorAll('.posts article.active');
+
+for(let activeArticle of activeArticles){
+  activeArticle.classList.remove('active');
+}
+
+/* get 'href' attribute from the clicked link */
+
+/* find the correct article using the selector (value of 'href' attribute) */
+
+/* add class 'active' to the correct article */
 }
 
 const links = document.querySelectorAll('.titles a');
