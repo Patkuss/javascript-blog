e0790cf9cdc7c88aac1a9dcf31b2f47bb2958348
Patryk Płaczek <patryk.placzek@wp.pl>
2019-08-11 12:29:13 +0200

    Add rest scripts

--- js/script.js
+++ js/script.js
@@ -8,6 +8,7 @@ document.getElementById('test-button').addEventListener('click', function(){
 */
 
 const titleClickHandler = function(event){
+    event.preventDefault();
     const clickedElement = this;
     console.log('Link was clicked!');
     console.log(event);
@@ -21,7 +22,7 @@ for(let activeLink of activeLinks){
   activeLink.classList.remove('active');
 }
 
-/* [IN PROGRESS] add class 'active' to the clicked link */
+/* [DONE] add class 'active' to the clicked link */
 
 clickedElement.classList.add('active');
 console.log('clickedElement:', clickedElement);
@@ -34,11 +35,18 @@ for(let activeArticle of activeArticles){
   activeArticle.classList.remove('active');
 }
 
-/* get 'href' attribute from the clicked link */
+/* [DONE] get 'href' attribute from the clicked link */
 
-/* find the correct article using the selector (value of 'href' attribute) */
+const articleSelector = clickedElement.getAttribute('href');
 
-/* add class 'active' to the correct article */
+/* [DONE] find the correct article using the selector (value of 'href' attribute) */
+
+const targetArticle = document.querySelector(articleSelector)
+console.log(targetArticle)
+
+/* [DONE] add class 'active' to the correct article */
+
+targetArticle.classList.add('active');
 }
 
 const links = document.querySelectorAll('.titles a');
