dac26e96fadc34cba7f3315f3d0a87610667b125
Patryk Płaczek <patryk.placzek@wp.pl>
2019-08-11 12:06:28 +0200

    Add adding active class to clicked link scrpit

--- js/script.js
+++ js/script.js
@@ -8,11 +8,12 @@ document.getElementById('test-button').addEventListener('click', function(){
 */
 
 const titleClickHandler = function(event){
+    const clickedElement = this;
     console.log('Link was clicked!');
     console.log(event);
 
 
-/* remove class 'active' from all article links  */
+/* [DONE] remove class 'active' from all article links  */
 
 const activeLinks = document.querySelectorAll('.titles a.active');
 
@@ -20,9 +21,12 @@ for(let activeLink of activeLinks){
   activeLink.classList.remove('active');
 }
 
-/* add class 'active' to the clicked link */
+/* [IN PROGRESS] add class 'active' to the clicked link */
 
-/* remove class 'active' from all articles */
+clickedElement.classList.add('active');
+console.log('clickedElement:', clickedElement);
+
+/* [DONE] remove class 'active' from all articles */
 
 const activeArticles = document.querySelectorAll('.posts article.active');
 
